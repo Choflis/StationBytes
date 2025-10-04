0e669d165b9039697ff737ddb57ca1f9342e9bda
Choflis <lluquecon@unsa.edu.pe>
2025-10-04 17:50:44 -0500

    Enrutamiento_Nuevo

--- StationByte_StellarAnniversary/src/App.jsx
+++ StationByte_StellarAnniversary/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import PantallaInicio from './pages/PantallaInicio/PantallaInicio.jsx'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import PantallaInicio from "./pages/PantallaInicio/PantallaInicio.jsx";
+import ZonaDeEntrenamiento from "./pages/ZonaDeEntrenamiento/ZonaDeEntrenamiento.jsx";
 
 function App() {
   return (
-    <div className="PantallaPrincipal">
-      <PantallaInicio />
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<PantallaInicio />} />
+      <Route path="/entrenamiento" element={<ZonaDeEntrenamiento />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;

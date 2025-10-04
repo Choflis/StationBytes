e9f8750b5c1adc49292336506a8e234feb197761
AjraHuacsoJeansAnthony <jajra@unsa.edu.pe>
2025-10-04 17:00:49 -0500

    Zonas

---
+++ StationByte_StellarAnniversary/src/pages/ZonaDeLaboratorios/ZonaDeLaboratorios.jsx
@@ -0,0 +1,12 @@
+import './ZonaLaboratorios.css'
+
+function ZonaLaboratorios() {
+  return (
+    <div className="zonaLaboratorios">
+      <h1>🧪 Zona de Laboratorios</h1>
+      <p>Hola, esta es la página de la zona de laboratorios.</p>
+    </div>
+  )
+}
+
+export default ZonaLaboratorios

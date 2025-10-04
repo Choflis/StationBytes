b680ce0502443a43ce9ecc8be0c654a8c44b59db
AjraHuacsoJeansAnthony <jajra@unsa.edu.pe>
2025-10-04 13:23:14 -0500

    Avance Pantalla

---
+++ StationByte_StellarAnniversary/src/pages/PantallaInicio/PantallaInicio.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import './pantallaInicio.css'
+
+function PantallaInicio() {
+  const [mostrarBoton, setMostrarBoton] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setMostrarBoton(true), 1200)
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <div className="pantallaInicio">
+      <h1 className="tituloJuego">STELLAR ANNIVERSARY</h1>
+      {mostrarBoton && (
+        <button className="botonJugar">Jugar</button>
+      )}
+    </div>
+  )
+}
+
+export default PantallaInicio

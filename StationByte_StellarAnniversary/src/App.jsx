d2c683694562b2c4ca9ddf7c0a830eed92f430eb
Fernando Garambel <fgarambel@unsa.edu.pe>
2025-10-04 23:42:12 -0500

    Rutas agregadas a App

--- StationByte_StellarAnniversary/src/App.jsx
+++ StationByte_StellarAnniversary/src/App.jsx
@@ -11,12 +11,22 @@ function App() {
   return (
     <div className="PantallaPrincipal">
       <Routes>
+        {/* Pantalla de inicio del juego */}
         <Route path="/" element={<PantallaInicio />} />
+        
+        {/* Secuencia de llegada a la estación (3 escenas animadas) */}
         <Route path="/llegada" element={<ZonaDeLlegada />} />
+        
+        {/* Zona principal - Harmony (hub central con efecto 3D) */}
         <Route path="/harmony" element={<ZonaHarmony />} />
+        
+        {/* Zonas específicas de la estación espacial */}
         <Route path="/cupula" element={<ZonaCupula />} />
         <Route path="/entrenamiento" element={<ZonaDeEntrenamiento />} />
         <Route path="/laboratorios" element={<ZonaDeLaboratorios />} />
+        
+        {/* Ruta 404 - Redirige a inicio si la ruta no existe */}
+        <Route path="*" element={<PantallaInicio />} />
       </Routes>
     </div>
   );
